24bb0b5cd7f06cc88c9a30ef70d56ecfe9750f3e
Gabriel Pinheiro de Campos <gabriel.pinheiro2001@hotmail.com>
2025-02-19 20:27:45 -0300

    ajustando url api

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,12 +1,12 @@
 import axios from "axios";
 
-const baseUrl = 'https://vozativa-badpdshcc4e7ddfv.brazilsouth-01.azurewebsites.net'
+const baseUrl = "https://vozativaapi20250219193436.azurewebsites.net";
 
 const api = axios.create({
-  baseURL: baseUrl,
-  headers: {
-    "Content-Type": "application/json",
-  },
+	baseURL: baseUrl,
+	headers: {
+		"Content-Type": "application/json",
+	},
 });
 
 export default api;
