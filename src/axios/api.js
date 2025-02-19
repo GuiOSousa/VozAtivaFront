12484b882abfd4a5dea0803394e3bcc3bc1acbad
Hanyel-Brenner <125314727+Hanyel-Brenner@users.noreply.github.com>
2025-02-19 20:50:02 -0300

    Update api.js

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = "https://vozativaapi20250219193436.azurewebsites.net";
+const baseUrl = 'https://vozativaapi20250219193436.azurewebsites.net'
 
 const api = axios.create({
 	baseURL: baseUrl,
