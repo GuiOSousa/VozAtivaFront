0d8e6f59460c054c630c25e41af5a3b014aee0a7
GuiOSousa <guilherme.olivesousa@gmail.com>
2025-07-20 17:07:16 -0300

    Updated Backend URL 2

--- src/axios/api.js
+++ src/axios/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const baseUrl = 'https://api-mongo-tlli.onrender.com/';
+const baseUrl = 'https://api-mongo-tlli.onrender.com';
 
 const api = axios.create({
 	baseURL: baseUrl,
